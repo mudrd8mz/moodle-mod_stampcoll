3bdf7104a53a2ed73d79aa3b39d46c36ccd4a7af
David Mudrak <david.mudrak@gmail.com>
2008-04-27 22:41:58 +0000

    file stampcoll.php was added on branch MOODLE_18_STABLE on 2008-04-27 22:46:35 +0000

--- lang/cs_utf8/stampcoll.php
+++
@@ -1,9 +0,0 @@
-<?PHP // $Id$ 
-      // stampcoll.php - created with Moodle 1.9 Beta + (2007082000)
-      // local modifications from http://localhost/%7Emudrd8mz/moodle-head
-
-
-$string['addstampbutton'] = 'Přidat';
-$string['confirmdel'] = 'Opravdu chcete odstranit toto razítko?';
-
-?>

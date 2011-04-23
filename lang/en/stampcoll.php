bfc95ac932f781cb35c34df7fb0d8107c2c9f9a7
David Mudrak <david@moodle.com>
2011-04-24 00:28:40 +0200

    Work on rewrite for 2.0 started - initial cleanup

---
+++ lang/en/stampcoll.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for Stamp collection module
+ *
+ * @package    mod
+ * @subpackage stampcollection
+ * @copyright  2007 David Mudrak <david@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addstampbutton'] = 'Add';
+$string['confirmdel'] = 'Do you really want to delete this stamp?';
+$string['deletestamp'] = 'Delete stamp';
+$string['displayzero'] = 'Display users with no stamps';
+$string['editstamps'] = 'Edit stamps';
+$string['givenby'] = 'From: {$a}';
+$string['givento'] = 'To: {$a}';
+$string['modulename'] = 'Stamp collection';
+$string['modulenameplural'] = 'Stamp collections';
+$string['nostampscollected'] = 'No stamps collected';
+$string['nostampsincollection'] = 'No stamps in this collection';
+$string['notallowedtoviewstamps'] = 'You are not allowed to view stamps in this collection';
+$string['nousers'] = 'No users';
+$string['numberofcollectedstamps'] = 'Number of collected stamps: {$a}';
+$string['numberofstamps'] = 'Stamps';
+$string['numberofyourstamps'] = 'Number of your stamps: {$a}';
+$string['ownstamps'] = 'Own stamps';
+$string['showupdateforms'] = 'Show forms to update stamps';
+$string['stampcoll:collectstamps'] = 'Collect stamps';
+$string['stampcoll:givestamps'] = 'Give stamps';
+$string['stampcoll:managestamps'] = 'Manage stamps';
+$string['stampcoll:viewotherstamps'] = 'View others\' stamps';
+$string['stampcoll:viewownstamps'] = 'View own stamps';
+$string['stampimage'] = 'Stamp image';
+$string['stampimageinfo'] = 'Recommended size is 35x35 pixels';
+$string['studentsperpage'] = 'Students per page';
+$string['timemodified'] = 'Last change';
+$string['updatestampbutton'] = 'Update';
+$string['viewstamps'] = 'View stamps';
--- version.php
+++ version.php
@@ -1,9 +1,31 @@
-<?php // $Id$
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-///  Stamp Collection module version
+/**
+ * Stamp collection module version
+ *
+ * @package    mod
+ * @subpackage stampcoll
+ * @copyright  2007 David Mudrak <david@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
-$module->version  = 2008022003;
-$module->requires = 2007101508;  // Requires this Moodle version - 1.9 Beta 4
-$module->cron     = 0;
+defined('MOODLE_INTERNAL') || die();
 
-?>
+$module->version    = 0;
+$module->requires   = 2011033002;
+$module->cron       = 0;
+$module->component  = 'mod_stampcoll';

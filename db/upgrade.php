fb3b85da6db1fe32d70d5bed4975bd6f3ab20124
David Mudrak <david@moodle.com>
2011-12-07 16:06:48 +0100

    Database scheme upgrade for the 2.0 release
    The list of changes is pretty well readable from the added lines in
    db/upgrade.php. Apart from some cosmetic changes and cleanups, the
    following changes are significant:
    
    * new field stampcoll_stamps::modifier
    * new field stampcoll_stamps::timecreated
    * the field stampcoll_stamps::giver now contains NULL if the stamp was
      given in anonymous mode under legacy 1.x versions of the module

--- db/upgrade.php
+++ db/upgrade.php
@@ -37,6 +37,8 @@ function xmldb_stampcoll_upgrade($oldversion = 0) {
 
     $dbman = $DB->get_manager();
 
+    //==== 1.9 upgrade line ====
+
     /**
      * Only upgrades from the version for Moodle 1.9 are supported
      */
@@ -82,6 +84,194 @@ function xmldb_stampcoll_upgrade($oldversion = 0) {
        $field = new xmldb_field('format', XMLDB_TYPE_INTEGER, '2', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0', 'intro');
        $dbman->rename_field($table, $field, 'introformat');
        upgrade_mod_savepoint(true, 2011070103, 'stampcoll');
+    }
+
+    //==== 2.0 upgrade line ====
+
+    /**
+     * Drop foreign keys and indices
+     */
+    if ($oldversion < 2011120700) {
+        $table = new xmldb_table('stampcoll_stamps');
+        $key = new xmldb_key('stampcollid', XMLDB_KEY_FOREIGN, array('stampcollid'), 'stampcoll', array('id'));
+        $dbman->drop_key($table, $key);
+
+        $table = new xmldb_table('stampcoll_stamps');
+        $index = new xmldb_index('userid', XMLDB_INDEX_NOTUNIQUE, array('userid'));
+        if ($dbman->index_exists($table, $index)) {
+            $dbman->drop_index($table, $index);
+        }
+
+        $table = new xmldb_table('stampcoll_stamps');
+        $index = new xmldb_index('giver', XMLDB_INDEX_NOTUNIQUE, array('giver'));
+        if ($dbman->index_exists($table, $index)) {
+            $dbman->drop_index($table, $index);
+        }
+
+        // stampcoll savepoint reached
+        upgrade_mod_savepoint(true, 2011120700, 'stampcoll');
+    }
+
+    /**
+     * Drop the anonymous field from the stampcoll table
+     */
+    if ($oldversion < 2011120701) {
+        $table = new xmldb_table('stampcoll');
+        $field = new xmldb_field('anonymous');
+        if ($dbman->field_exists($table, $field)) {
+            $dbman->drop_field($table, $field);
+        }
+        upgrade_mod_savepoint(true, 2011120701, 'stampcoll');
+    }
+
+    /**
+     * Change the nullability of field image on table stampcoll to null
+     */
+    if ($oldversion < 2011120702) {
+        $table = new xmldb_table('stampcoll');
+        $field = new xmldb_field('image', XMLDB_TYPE_CHAR, '255', null, null, null, null, 'introformat');
+        $dbman->change_field_notnull($table, $field);
+        upgrade_mod_savepoint(true, 2011120702, 'stampcoll');
+    }
+
+    /**
+     * Changing the default of field stampcollid on table stampcoll_stamps to drop it
+     */
+    if ($oldversion < 2011120703) {
+        $table = new xmldb_table('stampcoll_stamps');
+        $field = new xmldb_field('stampcollid', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, 'id');
+        $dbman->change_field_default($table, $field);
+        upgrade_mod_savepoint(true, 2011120703, 'stampcoll');
+    }
+
+    /**
+     * Changing the default of field userid on table stampcoll_stamps to drop it
+     */
+    if ($oldversion < 2011120704) {
+        $table = new xmldb_table('stampcoll_stamps');
+        $field = new xmldb_field('userid', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, 'stampcollid');
+        $dbman->change_field_default($table, $field);
+        upgrade_mod_savepoint(true, 2011120704, 'stampcoll');
+    }
+
+    /**
+     * Changing nullability of field giver on table stampcoll_stamps to null
+     */
+    if ($oldversion < 2011120705) {
+        $table = new xmldb_table('stampcoll_stamps');
+        $field = new xmldb_field('giver', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null, 'userid');
+        $dbman->change_field_notnull($table, $field);
+        upgrade_mod_savepoint(true, 2011120705, 'stampcoll');
+    }
+
+    /**
+     * Changing the default of field giver on table stampcoll_stamps to drop it
+     */
+    if ($oldversion < 2011120706) {
+        $table = new xmldb_table('stampcoll_stamps');
+        $field = new xmldb_field('giver', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null, 'userid');
+        $dbman->change_field_default($table, $field);
+        upgrade_mod_savepoint(true, 2011120706, 'stampcoll');
+    }
+
+    /**
+     * Since 2.0, the giver contains NULL instead of 0 if the stamp was originally given in 1.x anonymous mode
+     *
+     * This is mainly to prevent eventual issues with foreign key reference once we start
+     * using it.
+     */
+    if ($oldversion < 2011120707) {
+        $DB->set_field('stampcoll_stamps', 'giver', null, array('giver' => 0));
+        upgrade_mod_savepoint(true, 2011120707, 'stampcoll');
+    }
+
+    /**
+     * Changing sign of field timemodified on table stampcoll_stamps to unsigned
+     */
+    if ($oldversion < 2011120708) {
+        $table = new xmldb_table('stampcoll_stamps');
+        $field = new xmldb_field('timemodified', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null, 'timecreated');
+        $dbman->change_field_unsigned($table, $field);
+        upgrade_mod_savepoint(true, 2011120708, 'stampcoll');
+    }
+
+    /**
+     * Changing the default of field timemodified on table stampcoll_stamps to drop it
+     */
+    if ($oldversion < 2011120709) {
+        $table = new xmldb_table('stampcoll_stamps');
+        $field = new xmldb_field('timemodified', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null, 'timecreated');
+        $dbman->change_field_default($table, $field);
+        upgrade_mod_savepoint(true, 2011120709, 'stampcoll');
+    }
+
+    /**
+     * Add the field modifier to the stampcoll_stamps table
+     */
+    if ($oldversion < 2011120710) {
+        $table = new xmldb_table('stampcoll_stamps');
+        $field = new xmldb_field('modifier', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null, 'giver');
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+        upgrade_mod_savepoint(true, 2011120710, 'stampcoll');
+    }
+
+    /**
+     * Add the field timecreated to the stampcoll_stamps table - initially with default 0
+     */
+    if ($oldversion < 2011120711) {
+        $table = new xmldb_table('stampcoll_stamps');
+        $field = new xmldb_field('timecreated', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0', 'text');
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+        upgrade_mod_savepoint(true, 2011120711, 'stampcoll');
+    }
+
+    /**
+     * Set the initial value of the timecreated field for existing stamps
+     *
+     * We have no other option here but to pretend that the stamp was created when it was modified most recently
+     */
+    if ($oldversion < 2011120712) {
+        $DB->execute("UPDATE {stampcoll_stamps} SET timecreated = timemodified");
+        upgrade_mod_savepoint(true, 2011120712, 'stampcoll');
+    }
+
+    /**
+     * Drop the default value of the field timecreated - it was there just temporarily so we were able to add
+     * that field.
+     */
+    if ($oldversion < 2011120713) {
+        $table = new xmldb_table('stampcoll_stamps');
+        $field = new xmldb_field('timecreated', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, 'text');
+        $dbman->change_field_default($table, $field);
+        upgrade_mod_savepoint(true, 2011120713, 'stampcoll');
+    }
+
+    /**
+     * Regenerate the foreign keys
+     */
+    if ($oldversion < 2011120714) {
+
+        $table = new xmldb_table('stampcoll_stamps');
+        $key = new xmldb_key('fk_stampcollid', XMLDB_KEY_FOREIGN, array('stampcollid'), 'stampcoll', array('id'));
+        $dbman->add_key($table, $key);
+
+        $table = new xmldb_table('stampcoll_stamps');
+        $key = new xmldb_key('fk_userid', XMLDB_KEY_FOREIGN, array('userid'), 'user', array('id'));
+        $dbman->add_key($table, $key);
+
+        $table = new xmldb_table('stampcoll_stamps');
+        $key = new xmldb_key('fk_giver', XMLDB_KEY_FOREIGN, array('giver'), 'user', array('id'));
+        $dbman->add_key($table, $key);
+
+        $table = new xmldb_table('stampcoll_stamps');
+        $key = new xmldb_key('fk_modifier', XMLDB_KEY_FOREIGN, array('modifier'), 'user', array('id'));
+        $dbman->add_key($table, $key);
+
+        upgrade_mod_savepoint(true, 2011120714, 'stampcoll');
     }
 
     return true;
